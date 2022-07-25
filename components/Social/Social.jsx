ed46103822c78c1fe2fcde4d09c2a5a8ab22c5fb
Facynn <griecofacundo37@gmail.com>
2022-07-25 02:20:45 -0300

    Add redirections social

--- components/Social/Social.jsx
+++ components/Social/Social.jsx
@@ -3,16 +3,16 @@ import styles from '../../styles/all.module.css';
 export default function Social() {
     return (
         <div className={styles.cardSocial}>
-            <a href="https://www.instagram.com/facynnn/" target="_blank">
+            <a href="https://www.facyn.xyz/social/instagram" target="_blank">
                 <img src="/svg/icons/instagram.svg" alt="Instragram Icon" />
             </a>
-            <a href="https://www.youtube.com/channel/UCk9IaSW8sjdFNarHSEP_yNg" target="_blank">
+            <a href="https://www.facyn.xyz/social/youtube" target="_blank">
                 <img src="/svg/icons/youtube.svg" alt="YouTube Icon" />
             </a>
-            <a href="https://github.com/Facynn" target="_blank">
+            <a href="https://www.facyn.xyz/social/github" target="_blank">
                 <img src="/svg/icons/github.svg" alt="GitHub Icon" />
             </a>
-            <a href="https://discord.gg/M4mgKjfHXg" target="_blank">
+            <a href="https://www.facyn.xyz/social/discord" target="_blank">
                 <img src="/svg/icons/discord.svg" alt="Discord Icon" />
             </a>
         </div>
